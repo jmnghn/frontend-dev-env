38564a6831e99ddac21f57fc91563d0df90717ce
jngmnghn <jngmnghn@jngmnghnui-MacBookPro.local>
2020-03-27 15:15:40 +0900

    loader... css-loader, style-loader 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/app.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    mode: 'development',
+    entry: {
+        main: './src/app.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve('./dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'url-loader',
+                options: {
+                    publicPath: './dist/',
+                    name: '[name].[ext]?[hash]',
+                },
+            },
+        ],
+    },
+};
